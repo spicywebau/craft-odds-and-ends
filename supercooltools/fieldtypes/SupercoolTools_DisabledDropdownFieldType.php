7fc4bcce19dd320ae3d593d7c75cba0a8cd86cab
Josh Angell <josh@supercooldesign.co.uk>
2015-09-10 10:47:30 +0100

    Adding a disabled dropdown field type

---
+++ supercooltools/fieldtypes/SupercoolTools_DisabledDropdownFieldType.php
@@ -0,0 +1,57 @@
+<?php
+namespace Craft;
+
+/**
+ * Class SupercoolTools_DisabledDropdownFieldType
+ *
+ * @package   SupercoolTools
+ * @author    Josh Angell <josh@supercooldesign.co.uk>
+ * @copyright Copyright (c) 2015, Supercool Ltd
+ * @see       http://plugins.supercooldesign.co.uk
+ * @since     1.0
+ */
+
+class SupercoolTools_DisabledDropdownFieldType extends DropdownFieldType
+{
+
+	// Public Methods
+	// =========================================================================
+
+	/**
+	 * @inheritDoc IComponentType::getName()
+	 *
+	 * @return string
+	 */
+	public function getName()
+	{
+		return Craft::t('Dropdown (disabled)');
+	}
+
+	/**
+	 * @inheritDoc IFieldType::getInputHtml()
+	 *
+	 * @param string $name
+	 * @param mixed  $value
+	 *
+	 * @return string
+	 */
+	public function getInputHtml($name, $value)
+	{
+		$options = $this->getTranslatedOptions();
+
+		// If this is a new entry, look for a default option
+		if ($this->isFresh())
+		{
+			$value = $this->getDefaultValue();
+		}
+
+		return craft()->templates->render('_includes/forms/select', array(
+			'name'     => $name,
+			'value'    => $value,
+			'options'  => $options,
+			'disabled' => true,
+			'class' => 'disabled'
+		));
+	}
+
+}
