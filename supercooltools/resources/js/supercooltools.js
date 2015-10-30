6dd7185a67f45eef98ba494168021e09366ce776
Josh Angell <josh@supercooldesign.co.uk>
2015-10-30 13:41:04 +0000

    Remove @since docblock tag as we’re not using it.

--- supercooltools/resources/js/supercooltools.js
+++ supercooltools/resources/js/supercooltools.js
@@ -2,7 +2,6 @@
  * @author    Josh Angell <josh@supercooldesign.co.uk>
  * @copyright Copyright (c) 2015, Supercool Ltd
  * @see       http://plugins.supercooldesign.co.uk
- * @since     1.0
  */
 
 (function($){
