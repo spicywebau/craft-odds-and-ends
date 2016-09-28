99beae3f6a168b988fd66e5dce7a11b77b602cbc
Naveed <naveed@supercooldesign.co.uk>
2016-09-28 09:43:07 +0100

    Fix model rules and default value

--- supercooltools/models/SupercoolTools_DefaultNumberFieldTypeSettingsModel.php
+++ supercooltools/models/SupercoolTools_DefaultNumberFieldTypeSettingsModel.php
@@ -2,16 +2,35 @@
 
 namespace Craft;
 
+/**
+ * Class SupercoolTools_DefaultNumberFieldTypeSettingsModel
+ *
+ * @package   SupercoolTools
+ * @author    Naveed Ziarab <naveed@supercooldesign.co.uk>
+ * @copyright Copyright (c) 2016, Supercool Ltd
+ * @see       http://plugins.supercooldesign.co.uk
+ */
+
 class SupercoolTools_DefaultNumberFieldTypeSettingsModel extends NumberFieldTypeSettingsModel
 {
 
+	/**
+	 * @inheritDoc BaseModel::rules()
+	 * 
+	 * @return array
+	 */
 	public function rules()
 	{
 		$rules = parent::rules();
-
+		$rules[] = array('defaultNumber', 'required');
 		return $rules;
 	}
 
+	/**
+	 * @inheritDoc BaseModel::defineAttributes()
+	 * 
+	 * @return array
+	 */
 	protected function defineAttributes()
 	{
 		$attributes = parent::defineAttributes();
