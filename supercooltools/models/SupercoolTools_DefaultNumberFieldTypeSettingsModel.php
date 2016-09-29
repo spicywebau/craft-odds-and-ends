3350fb900ea964fc6c5be56339c0e8ceffe9dcc9
Naveed <naveed@supercooldesign.co.uk>
2016-09-29 16:35:01 +0100

    Add min and max validation

--- supercooltools/models/SupercoolTools_DefaultNumberFieldTypeSettingsModel.php
+++ supercooltools/models/SupercoolTools_DefaultNumberFieldTypeSettingsModel.php
@@ -15,18 +15,6 @@ class SupercoolTools_DefaultNumberFieldTypeSettingsModel extends NumberFieldType
 {
 
 	/**
-	 * @inheritDoc BaseModel::rules()
-	 * 
-	 * @return array
-	 */
-	public function rules()
-	{
-		$rules = parent::rules();
-		$rules[] = array('defaultNumber', 'required');
-		return $rules;
-	}
-
-	/**
 	 * @inheritDoc BaseModel::defineAttributes()
 	 * 
 	 * @return array
@@ -39,4 +27,52 @@ class SupercoolTools_DefaultNumberFieldTypeSettingsModel extends NumberFieldType
 
 		return $attributes;
 	}
+
+
+	/**
+	 * @inheritDoc BaseModel::rules()
+	 * 
+	 * @return array
+	 */
+	public function rules()
+	{
+		$rules = parent::rules();
+		$rules[] = array('defaultNumber', 'required');
+		$rules[] = array('defaultNumber', 'validateDefaultNumber');
+
+		if( $this->decimals && intval($this->decimals) && intval($this->decimals) > 0 ) 
+		{
+			foreach( $rules as $key => &$rule ) 
+			{
+				if( $rule[0] == 'defaultNumber' && $rule[1] == 'numerical' ) 
+				{
+					$rule['integerOnly'] = false;
+				}
+			}
+		}
+
+
+		return $rules;
+	}
+
+
+	/**
+	 * 
+	 */
+	public function validateDefaultNumber( $attribute )
+	{
+		$value = $this->defaultNumber;
+		$min = $this->min;
+		$max = $this->max;
+
+		if( $value < $min ) {
+			$message = Craft::t("Number must be equal or greater than ".$min);
+			$this->addError($attribute, $message);
+		} elseif( $max != "" && $value > $max ) {
+			$message = Craft::t("Number must be equal or less than ".$max);
+			$this->addError($attribute, $message);
+		}
+
+	}
+
 }
