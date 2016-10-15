50936338c3863642ac37d8bac121309e0be396af
Naveed <naveed.ziarab@gmail.com>
2016-10-15 13:27:08 +0100

    Implement retain state feature and redesign the whole slider

--- supercooltools/tools/WidthData.php
+++ supercooltools/tools/WidthData.php
@@ -16,6 +16,11 @@ class WidthData
 	public $left;
 	public $right;
 
+	public $firstPointer;
+	public $secondPointer;
+	public $thirdPointer;
+	public $fourthPointer;
+
 	public $widthOptions;
 	public $leftOptions;
 	public $rightOptions;
@@ -25,7 +30,11 @@ class WidthData
 								$rightOptions = null,
 								$width = null,
 								$left = null,
-								$right = null)
+								$right = null,
+								$firstPointer = null,
+								$secondPointer = null,
+								$thirdPointer = null,
+								$fourthPointer = null)
 	{
 		$this->widthOptions = $widthOptions;
 		$this->leftOptions = $leftOptions;
@@ -34,6 +43,11 @@ class WidthData
 		$this->width = $width;
 		$this->left = $left;
 		$this->right = $right;
+
+		$this->firstPointer = $firstPointer;
+		$this->secondPointer = $secondPointer;
+		$this->thirdPointer = $thirdPointer;
+		$this->fourthPointer = $fourthPointer;
 	}
 
 	public function __toString()
@@ -51,6 +65,13 @@ class WidthData
 		$width = "";
 		$left  = "";
 		$right = "";
+
+		$firstPointer = 0;
+		$secondPointer = 0;
+		$thirdPointer = 0;
+		$fourthPointer = 0;
+
+		$total = count($options);
 
 		foreach ($options as $option) 
 		{
@@ -73,15 +94,46 @@ class WidthData
 				$right = $option['rightValue'];
 			}
 		}
+		// Set Keys and pointers
+		$widthKey = array_search($width, $widthOptions);
+		$leftKey = array_search($left, $leftOptions);
+		$rightKey = array_search($right, $rightOptions);
+
+		if ( $leftKey !== false ) {
+			$leftKey = $leftKey + 1;
+
+			$firstPointer = $leftKey - $total;
+			$secondPointer = $leftKey;
+		}
+
+		if ( $widthKey !== false ) {
+			$widthKey = $widthKey + 1;
+
+			$thirdPointer = $leftKey + $widthKey;
+		}
+
+		if ( $rightKey !== false ) {
+			$rightKey = $rightKey + 1;
+
+			$fourthPointer = ($leftKey + $widthKey) + $rightKey;
+		}
+
+		
 
 		if ( $value )
 		{
 			$width = $value['width'];
 			$left  = $value['left'];
 			$right = $value['right'];
+
+			$firstPointer = $value['firstPointer'];
+			$secondPointer = $value['secondPointer'];
+			$thirdPointer = $value['thirdPointer'];
+			$fourthPointer = $value['fourthPointer'];
 		}
 
-		return new self( $widthOptions, $leftOptions, $rightOptions, $width, $left, $right );
+		return new self( $widthOptions, $leftOptions, $rightOptions, $width, $left, $right,
+						$firstPointer, $secondPointer, $thirdPointer, $fourthPointer );
 	}
 
 }
